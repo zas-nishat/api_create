0494fcd41ed6ee751dbdf9e82282e0313ae3bebc
Nishat <104446439+zas-nishat@users.noreply.github.com>
2024-12-23 18:19:55 +0600

    delete user added

--- api_site/add_data/urls.py
+++ api_site/add_data/urls.py
@@ -3,6 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('users/', views.createUserList.as_view() , name= "user-create-view"),
-    path('users/update/<int:pk>/', views.retrieveUpdateDestroyUserList.as_view(), name= 'update')  
+    path('users/', views.showUserLists.as_view() , name= "create-user"),    
+    path('create/', views.createUserList.as_view() , name= "create-user"),
+    path('update/<int:pk>/', views.updateUserList.as_view(), name= 'update') ,
+    path('delete/<int:pk>/', views.deleteUserList.as_view(), name= 'delete')  
 ]
+
+
+# urlpatterns = [
+#     path('users/', get_users , name= "users"),
+#     path('users/create/', create_users , name= "new_users"),
+# ]
--- api_site/add_data/views.py
+++ api_site/add_data/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 from rest_framework import generics,status
 from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
 
+
+class showUserLists (generics.ListAPIView ):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class createUserList (generics.ListCreateAPIView ):
     queryset = User.objects.all()
@@ -12,7 +17,32 @@ class createUserList (generics.ListCreateAPIView ):
         User.objects.all().delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class retrieveUpdateDestroyUserList(generics.RetrieveUpdateDestroyAPIView):
+class updateUserList(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = "pk"
+    
+class deleteUserList(generics.RetrieveDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "pk"
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"message": "User deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+
+
+# @api_view(['GET'])
+# def get_users(request):
+#     users = User.objects.all()
+#     serializer = UserSerializer(users, many = True)
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def create_users(request):
+#     serializer = UserSerializer(data= request.data)
+#     if UserSerializer.is_valid:
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
